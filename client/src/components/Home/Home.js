1db256fd0968725c9973d0154c44eb5ff9ffa431
unknown <aishwaryaaj2002@gmail.com>
2021-09-16 13:22:52 +0530

    installed material-ui

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import {React} from "react";
+import Leftbar from "../Leftbar/Leftbar";
+
+function Home() {
+    return (
+      <div >
+         <Leftbar/> 
+      </div>
+    );
+  }
+  
+  export default Home;
---
+++ client/src/components/Leftbar/Leftbar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { Drawer } from '@material-ui/core';
+ 
+const Leftbar=() => {
+        return (
+            <Drawer  variant="persistent" anchor="left">
+               hello
+            </Drawer>
+        );
+}
+
+export default Leftbar;
