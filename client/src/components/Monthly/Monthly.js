70d944e7e293059e8e1bdd05bfd9cd4530e19e03
ireneelizabethsabu <irenepalanatt@gmail.com>
2021-09-25 21:48:14 +0530

    adding line chart

--- client/src/components/Monthly/Monthly.js
+++ client/src/components/Monthly/Monthly.js
@@ -1,36 +1,59 @@
 import React from "react";
-import { VictoryChart,VictoryLine,VictoryTooltip } from "victory";
+import { Container } from "react-bootstrap";
+import { Line } from "react-chartjs-2";
 import { useActivity } from "../../hooks/useActivity";
 import moment from "moment";
 
 const Monthly = () => {
    const { getMonthSummary } =  useActivity();
-   const data = getMonthSummary('',moment())
-  return (
+   const summary = getMonthSummary('',moment())
+    
+   const data = {
+    labels: summary.map(ele => ele.day),
+    datasets: [
+      {
+        label: "Expense",
+        data: summary.map(ele => ele.expense),
+        fill: false,
+        backgroundColor: "#333333",
+        borderColor: "#333333",
+        pointHoverBackgroundColor: "#082D39",
+        tension: 0.1,
+        borderWidth: 3,
+      },
+      {
+        label: "Income",
+        data: summary.map(ele => ele.income),
+        fill: false,
+        backgroundColor: "#4287f5",
+        borderColor: "#4287f5",
+        pointHoverBackgroundColor: "#4287f5",
+        tension: 0.1,
+        borderWidth: 3,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false, 
+    plugins: {
+      tooltip: {
+        backgroundColor: "#fff",
+        displayColors: true,
+        titleColor: "#082D39",
+        bodyColor: "#082D39",
+        padding: 12,
+      },
+    },
+  };
+
+   return (
     <div>
       <h1>Monthly Status</h1>
-      <VictoryChart >
-          
-        <VictoryLine
-          style={{
-            data: { stroke: "#c43a31" },
-            parent: { border: "1px solid #ccc" },
-          }}
-          data={data}
-          x="day"
-          y="expense"
-          
-        
-        />
-        <VictoryLine
-          style={{
-            data: { stroke: "#000" }
-          }}
-          data={data}
-          
-          y="income"
-        />
-      </VictoryChart>
+      <Container fluid className="py-5 px-0">
+        <Line  data={data} options={options} />
+      </Container>
     </div>
   );
 };
