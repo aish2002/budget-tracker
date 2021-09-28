dbd2075418868c64314e39068bc45bd39e7d346d
ireneelizabethsabu <irenepalanatt@gmail.com>
2021-09-28 19:23:07 +0530

    configurations for production

--- server/index.js
+++ server/index.js
@@ -8,11 +8,30 @@ import {connect} from './config/database';
 import { addactivity, getactivity } from './controllers/activity';
 import {setbudget,getbudget} from './controllers/budget';
 import session from 'express-session';
+import bodyParser from 'body-parser';
 
 connect();
 const app = express();
 //for serversiderendering
-app.use(express.static(path.resolve(__dirname,'../client/build')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+//app.use(cors());
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+};
+
+app.get('*', (req, res) => {
+    res.status(200).json({
+        msg: 'Catch All'
+    });
+});
+
 app.use(express.json())
 app.use(session({
   secret: 'sfjsk,akqklqkqkel',
@@ -33,15 +52,10 @@ app.get('/api/getactivity',getactivity);
 app.post('/api/setbudget',setbudget);
 app.get('/api/getbudget',getbudget);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, (err) => {
-    console.log(`Server listening on ${PORT}`);
-    if(err){
-      console.log(err);
-      return;
-    }
-});
+
+app.listen(PORT, () => console.log(`BACK_END_SERVICE_PORT: ${PORT}`));
 
 //for production
 app.get('*',(req,res) => 
-    res.sendFile(path.resolve(__dirname,'../client/build','index.html'))
+    res.sendFile(path.resolve(__dirname,'client/build','index.html'))
 )
