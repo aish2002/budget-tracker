ddc174834e83a4ab45bbac60e39dcee61e4a84f9
ireneelizabethsabu <irenepalanatt@gmail.com>
2021-09-28 19:00:26 +0530

    fixed warning

--- client/src/components/Leftbar/Leftbar.js
+++ client/src/components/Leftbar/Leftbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar,Nav,NavbarBrand} from "react-bootstrap";
+import { Navbar,Nav} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Leftbar.css";
 
